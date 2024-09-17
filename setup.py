522b136d712ce41d5dfedd26a978663c3adc80d7
Shibaji Chakraborty <shibaji7@vt.edu>
2024-09-17 16:10:24 -0400

    Setup codebase 09/17

---
+++ setup.py
@@ -0,0 +1,32 @@
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="pynasonde",
+    version="0.1",
+    packages=["pynasonde"],
+    package_dir={"pynasonde": "pynasonde"},
+    package_data={"pynasonde": []},
+    author="Shibaji Chakraborty",
+    author_email="chakras4@erau.edu",
+    maintainer="Shibaji Chakraborty",
+    maintainer_email="chakras4@erau.edu",
+    license="GNU GPL License",
+    description=long_description,
+    long_description=long_description,
+    install_requires=[],
+    keywords=["python", "ionosonde", "dynasonde"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/shibaji7/pynasonde",
+)
