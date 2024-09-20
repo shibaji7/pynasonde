96f3b89b2203f83a78ef70356005473926fafb9c
Shibaji Chakraborty <shibaji7@vt.edu>
2024-09-20 14:04:14 -0400

    Updated Webhook

--- code/dump_header.py
+++ code/dump_header.py
@@ -1,20 +1,25 @@
-from pynasonde.ngi.ionograms import DataSource
+# from pynasonde.ngi.ionograms import DataSource
 
-fname = "tmp/WI937_2024264001803.RIQ"
-from pynasonde.riq.headers.pct import PctType
-from pynasonde.riq.headers.sct import SctType
+# fname = "tmp/WI937_2024264001803.RIQ"
+# from pynasonde.riq.headers.pct import PctType
+# from pynasonde.riq.headers.sct import SctType
 
-x, y = SctType(), PctType()
-x.read_sct(fname)
-x.dump_sct("tmp/WI937_2024264001803_sct.txt")
-y.load_sct(x)
-y.read_pct(fname)
-y.dump_pct("tmp/WI937_2024264001803_pct.txt")
+# x, y = SctType(), PctType()
+# x.read_sct(fname)
+# x.dump_sct("tmp/WI937_2024264001803_sct.txt")
+# y.load_sct(x)
+# y.read_pct(fname)
+# y.dump_pct(to_file="tmp/WI937_2024264001803_pct.txt")
 
 
-ds = DataSource()
-# print(ds.file_paths)
-ds.load_data_sets()
-print(ds.datasets[0].X_mode_power)
-# print(ds.extract_FTI_RTI())
-ds.extract_ionograms()
+# ds = DataSource()
+# # print(ds.file_paths)
+# ds.load_data_sets()
+# print(ds.datasets[0].X_mode_power)
+# # print(ds.extract_FTI_RTI())
+# ds.extract_ionograms()
+
+
+from pynasonde.webhook import Webhook
+
+Webhook().download()
--- pynasonde/webhook.py
+++ pynasonde/webhook.py
@@ -1,18 +1,54 @@
 import datetime as dt
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field
 from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+from loguru import logger
 
 
 @dataclass
 class Webhook:
     URSI: str = "WI937"
-    dates: List[dt.datetime] = None
-    raw_url: str = "http://wallops.ionosonde.net/archive/individual/{year}/{doy}/raw/"
-    raw_file_name: str = "{URSI}_{year}{doy}{hour}{min}{sec}.RIQ"
+    dates: List[dt.datetime] = field(default_factory=lambda: [dt.datetime(2024, 9, 20)])
+    riq_url: str = "http://wallops.ionosonde.net/archive/individual/{year}/{doy}/raw/"
+    riq_file_name: str = "{URSI}_{year}{doy}{hour}{min}{sec}.RIQ"
     ngi_url: str = (
         "http://wallops.ionosonde.net/archive/individual/{year}/{doy}/ionogram/"
     )
     ngi_file_name: str = "{URSI}_{year}{doy}{hour}{min}{sec}.ngi"
 
-    def __download__(self):
+    def download(self, source: str = "./tmp/"):
+        for d in self.dates:
+            source = os.path.join(source, d.strftime("%Y%m%d"))
+            os.makedirs(source, exist_ok=True)
+            ngi_url = self.ngi_url.format(year=d.year, doy=d.timetuple().tm_yday)
+            self.__dump_files__(ngi_url, source, ".ngi")
+            riq_url = self.riq_url.format(year=d.year, doy=d.timetuple().tm_yday)
+            self.__dump_files__(riq_url, source, ".RIQ")
+        return
+
+    def __dump_files__(self, url: str, source: str, ext: str):
+        logger.info(f"Checking: {url}")
+        r = requests.get(url)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, "lxml")
+            table = soup.find_all("a", href=True)
+            for row in table:
+                href = row.attrs["href"]
+                if (self.URSI in href) and (
+                    (ext.lower() in href) or (ext.upper() in href)
+                ):
+                    url = url + href
+                    rx = requests.get(url)
+                    if r.status_code == 200:
+                        file = os.path.join(source, href)
+                        with open(file, "wb") as f:
+                            f.write(rx.content)
+                    else:
+                        logger.info(f"Error in downloading: {href} / {rx.status_code}")
+                    break
+        else:
+            logger.info(f"Error in logging: {url} / {r.status_code}")
         return
